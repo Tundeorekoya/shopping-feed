d989814f9a36f2ad5224593156c5f07442526aa5
tundeorekoya <tundeorekoya1998@gmail.com>
2023-11-25 00:57:12 -0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
-import Home from "./scenes/home/Home";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation"
 import Navbar from "./scenes/global/Navbar";
 import CartMenu from "./scenes/global/CartMenu";
+import Home from "./scenes/home/Home";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -24,7 +24,7 @@ function App() {
       <Navbar/>
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
           <Route path="item/:itemId" element={<ItemDetails />} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="check/success" element={<Confirmation/>} />
