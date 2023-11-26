d42940b9edba547f90907b7743315b122a980b48
tundeorekoya <tundeorekoya1998@gmail.com>
2023-11-26 01:01:12 -0800

    update

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,12 +1,6 @@
 import { React, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Box,
-  Typography,
-  Tab,
-  Tabs,
-  useMediaQuery,
-} from "@mui/material";
+import { Box, Typography, Tab, Tabs, useMediaQuery } from "@mui/material";
 import Items from "../../component/Items";
 import { setItems } from "../../state";
 
@@ -21,18 +15,69 @@ const ShoppingList = () => {
     SetValue(newValue);
   };
 
- async function getItems() {
-   const items = await fetch("http://localhost:1337/api/items?populate=image", {
-     method: "GET",
-   });
-   const itemsJson = await items.json();
-   dispatch(setItems(itemsJson.data));
- }
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:1337/api/items?populate=image",
+      {
+        method: "GET",
+      }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
 
   useEffect(() => {
     getItems();
   }, []);
-  return <div>shopping List</div>;
+  const topRatedItems = items.filter(
+    (item) => item.attributes.category === "topRated"
+  );
+  const newArrivalsItems = items.filter(
+    (item) => item.attributes.category === "newArrivals"
+  );
+  const bestSellerItems = items.filter(
+    (item) => item.attributes.category === "bestSeller"
+  );
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Our Featured <b>Product</b>
+      </Typography>
+
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{ sx: { display: isNonMobile ? "block" : "none" } }}
+        sx={{
+          m: "25px",
+          "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+          },
+        }}
+      >
+        <Tab label="ALL" value="all" />
+        <Tab label="NEW ARRIVALS" value="newArrivals" />
+        <Tab label="BEST SELLERS" value="bestSellers" />
+        <Tab label="TOP RATED" value="topRated" />
+      </Tabs>
+      <Box
+      margin=" 0 auto"
+      display= "grid"
+      gridTemplateColumns="repeat(auto-fill, 300px)"
+      justifyContent="space-around"
+      rowGap="20px"
+      columnGap="1.33%"
+      >
+        {value ==="all" && items.map((items) => (
+            <Items item={items} key={`${items.name}-${items.id}`}/>
+            
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default ShoppingList;
